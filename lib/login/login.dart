ccaf180a3537c2c3e6eb59189e0aeb5cbf3a941e
Axel Rodriguez <alejandrom9712@gmail.com>
2020-07-11 17:37:09 -0600

    Prueba de fondo animado

---
+++ lib/login/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlareActor(
+      "assts/login_background.flr",
+    );
+  }
+}
